7fc7f8ae8f1bb3c7e1c4671a1a10206e24a71de1
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-27 18:05:31 +0300

    Task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
-    let task2 = false;
+    var task = true;
+    var task2 = false;
   }
 
   return [task, task2];
