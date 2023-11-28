1c3c4bb2324da1821ecbd3fe144741b16fe87092
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-28 16:01:03 +0300

    Fix

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
 export function taskFirst() {
   const task = "I prefer const when I can.";
   return task;
---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,7 @@
+export default function getNeighborhoods() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+    const self = this;
+    this.addNeighborhood(newNeighborhood) => self.sanFranciscoNeighborhoods.push(newNeighborhood); 
+    self.sanFranciscoNeighborhoods;
+}
