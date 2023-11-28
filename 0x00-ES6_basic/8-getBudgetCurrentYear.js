ebd4a9fbecf3f8df608504ffd23b707334c36191
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-28 23:03:06 +0300

    Fix

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,12 +1,12 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const budget = {};
-  function getCurrentYear() {
-    return new Date().getFullYear();
-  }
-
-  budget[`income-${getCurrentYear()}`] = income;
-  budget[`gdp-${getCurrentYear()}`] = gdp;
-  budget[`capita-${getCurrentYear()}`] = capita;
-
-  return budget;
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 }
