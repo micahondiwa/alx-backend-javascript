a02d8aa626f5717c3b434cfb4f282c78661feed3
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-28 18:11:46 +0300

    Fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,14 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
+    const temTask = task;
+    const tempTask2 = task2;
+
     task = true;
     task2 = false;
+
+    task = temTask;
+    task2 = tempTask2;
   }
 
   return [task, task2];
