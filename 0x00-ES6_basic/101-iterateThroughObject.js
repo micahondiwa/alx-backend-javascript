3cd8163c91dc6c2671492ef6b3f90f247a9de308
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-29 14:43:42 +0300

    Fixed

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,4 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-  const employeeNames = Array.from(reportWithIterator);
+  const employeeNames = [];
+  for (const department of reportWithIterator.allEmployees) {
+    const employeesInDepartment = reportWithIterator.allEmployees[department];
+    employeeNames.push(...employeesInDepartment);
+  }
   return employeeNames.join(' | ');
 }
