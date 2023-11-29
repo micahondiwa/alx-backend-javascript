7a00011efc0d80cba7e648c8bb8333a632276080
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-29 09:55:00 +0300

    Fix

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+  for (const value of array) {
+    const idx = array.indexOf(value);
+    array[idx] = appendString + value; // eslint-disable-line no-param-reassign
   }
 
   return array;
