ab4f1e7e075b653bb4fe50d10deda093c52dcfe4
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-11-29 14:54:29 +0300

    Fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,9 +1,5 @@
-export default function iterateThroughObject(reportWithIterator) {
-  let allEmployeesString = '';
-  for (const department of reportWithIterator) {
-    for (const employee of reportWithIterator[department]) {
-      allEmployeesString += `${employee} | `;
-    }
-  }
-  return allEmployeesString.slice(0, -2); // Remove the trailing ' | '
+export default function createEmployeesObject(departmentName, employees) {
+  const department = {};
+  department[departmentName] = employees;
+  return department;
 }
