5bfe9a95a1f8bd9e190a4a086f9f4b4424dc68d2
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-03 23:44:57 +0300

    Final

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName,
+) {
+  const userData = await signUpUser(firstName, lastName).then((data) => ({
+    status: 'fulfiled',
+    value: data,
+  }));
+
+  const fileData = await uploadPhoto(fileName).catch((err) => ({
+    status: 'rejected',
+    value: err.toString(),
+  }));
+
+  return Promise.resolve([userData, fileData]);
+}
