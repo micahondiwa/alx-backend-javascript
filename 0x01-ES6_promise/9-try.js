5172b211ef2a1d5fad13a5def459fe97a62f3f1e
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-04 00:01:21 +0300

    Try

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,10 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    queue.push(mathFunction());
+  } catch (e) {
+    queue.push(e.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+}
