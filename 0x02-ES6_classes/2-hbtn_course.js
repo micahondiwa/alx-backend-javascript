b5a941673fcc66886d5a7e2f91362c8f6b955383
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-04 18:42:06 +0300

    Hot Fix

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-underscore-dangle */
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Length must be a number');
+    }
+    this._length = value;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(value) {
+    if (typeof value !== 'object') {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
