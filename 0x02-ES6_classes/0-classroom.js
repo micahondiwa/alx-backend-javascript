41a8a123f15753efd70f86d2d6912c953ca57319
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-04 17:18:46 +0300

    Fix

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,6 @@
 export default class ClassRoom {
-    constructor(maxStudentsSize); {
-    this.maxStudentsSize = _maxStudentsSize;
-    }
+  constructor(maxStudentsSize) {
+    // eslint-disable-next-line no-underscore-dangle
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
