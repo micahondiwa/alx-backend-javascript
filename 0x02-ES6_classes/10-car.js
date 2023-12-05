72a4935ba18db0d4044112c85cb4ad39675f4b24
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-05 23:01:31 +0300

    Cars

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const NewObj = this.constructor[Symbol.species] || this.constructor;
+    const clone = new NewObj();
+    return clone;
+  }
+}
