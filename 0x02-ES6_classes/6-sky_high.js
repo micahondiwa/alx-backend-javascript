2d8306ebc69064124e351f07e4cdc3926ccdab67
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-05 11:09:39 +0300

    Sky high building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-underscore-dangle */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    this._sqft = sqft;
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof (value) === 'number') {
+      this._floors = value;
+    }
+  }
+
+  /* eslint-disable-next-line class-methods-use-this */
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
