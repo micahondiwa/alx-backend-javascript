74d5de59512d1b58c5b4207bb9456ae9186ff6f8
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-05 11:12:02 +0300

    RTFM

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,7 +3,7 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    this._sqft = sqft;
+    super(sqft);
     this._floors = floors;
   }
 
