afbc8c68848e46ac4aa251cba4e05376768891fc
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-05 10:42:18 +0300

    Pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof amount === 'number') {
+      this._amount = value;
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullPrice()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
