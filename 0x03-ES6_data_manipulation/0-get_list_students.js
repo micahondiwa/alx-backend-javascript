7c78d1ebdf83106f9569a12b84e4edb3b8f09b69
micahondiwa <micah.oketch.ondiwa@gmail.com>
2023-12-07 22:03:45 +0300

    List of students

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,3 +1,5 @@
+/* Get list of students */
+
 export default function getListStudents() {
   return [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' }, { id: 2, firstName: 'James', location: 'Columbia' }, { id: 5, firstName: 'Serena', location: 'San Francisco' }];
 }
