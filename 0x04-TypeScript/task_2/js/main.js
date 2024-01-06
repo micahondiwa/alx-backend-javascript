1be8ab48bec43f63d320107056dc098967a7393a
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-06 15:12:11 +0300

    Task 2

--- 0x04-TypeScript/task_2/js/main.js
+++ 0x04-TypeScript/task_2/js/main.js
@@ -1,27 +1,63 @@
 "use strict";
 /**
- * Building a teacher interface with elements:
- * firstName(string), lastName(string), fullTimeEmployee(boolean),
- * yearsOfExperience(number), location(string)
+ * creating the DirectorInterface
  */
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.StudentClass = exports.printTeacher = void 0;
-;
-function printTeacher(firstName, lastName) {
-    return "".concat(firstName[0], ". ").concat(lastName);
-}
-exports.printTeacher = printTeacher;
-var StudentClass = /** @class */ (function () {
-    function StudentClass(firstName, lastName) {
-        this._firstName = firstName;
-        this._lastName = lastName;
+exports.teachClass = exports.executeWork = exports.isDirector = exports.CreateEmployee = exports.Teacher = exports.Director = void 0;
+var Director = /** @class */ (function () {
+    function Director() {
     }
-    StudentClass.prototype.workOnHomework = function () {
-        return 'Currently working';
+    Director.prototype.workFromHome = function () {
+        return 'Working from home';
     };
-    StudentClass.prototype.displayName = function () {
-        return this._firstName;
+    Director.prototype.getCoffeeBreak = function () {
+        return 'Getting a coffee break';
     };
-    return StudentClass;
+    Director.prototype.workDirectorTasks = function () {
+        return 'Getting to director tasks';
+    };
+    return Director;
 }());
-exports.StudentClass = StudentClass;
+exports.Director = Director;
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return 'Cannot work from home';
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return 'Cannot have a break';
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return 'Getting to work';
+    };
+    return Teacher;
+}());
+exports.Teacher = Teacher;
+function CreateEmployee(salary) {
+    if (typeof salary === 'number' && salary < 5000) {
+        return new Teacher();
+    }
+    return new Director();
+}
+exports.CreateEmployee = CreateEmployee;
+function isDirector(employee) {
+    return employee instanceof Director;
+}
+exports.isDirector = isDirector;
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        return employee.workDirectorTasks();
+    }
+    return employee.workTeacherTasks();
+}
+exports.executeWork = executeWork;
+function teachClass(todayClass) {
+    if (todayClass === 'Math') {
+        return 'Teaching Math';
+    }
+    if (todayClass === 'History') {
+        return 'Teaching History';
+    }
+}
+exports.teachClass = teachClass;
