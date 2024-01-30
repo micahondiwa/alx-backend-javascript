6604c31109b607c6b6abbd1ce3bf29ad5cb1cb3f
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-01-30 22:39:25 +0300

    F

---
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: ["airbnb-base", "plugin:jest/all"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: ["jest"],
+  rules: {
+    "no-console": "off",
+    "no-shadow": "off",
+    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
+    "import/extensions": ["error", "ignorePackages", { js: "never" }],
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      excludedFiles: "babel.config.js",
+    },
+  ],
+};
