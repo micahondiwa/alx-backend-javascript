5ae1b7926651a9468708847e10235984e72114d7
micahondiwa <micah.oketch.ondiwa@gmail.com>
2024-02-01 22:15:37 +0300

    fix

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -7,11 +7,11 @@ describe('calculateNumber', () => {
     });
 
     it('rounding down b\'s floating point fractional number', () => {
-        assert.strictEqual(calculateNumber(1.0, 2.4, 3));
+        assert.strictEqual(calculateNumber(1.0, 2.4), 3);
     });
 
-    it('rounding down a b\'s floating point fractional number', () => {
-        assert.strictEqual(calculateNumber(1.4, 2.0), 3);
+    it('rounding down a and b\'s floating point fractional number', () => {
+        assert.strictEqual(calculateNumber(1.4, 2.4), 3);
     });
 
     it('rounding down a\'s floating point fractional number', () => {
@@ -33,4 +33,4 @@ describe('calculateNumber', () => {
     it('rounding down a and b floating point fractional numbers with trailing 9\'s', () => {
         assert.strictEqual(calculateNumber(2.499999, 3.499999), 5);
     });
-})
+});
